2eb929693c0973d7d105ca7ca4d593b57ce69885
Ömer Osman <113211889+omerosmann@users.noreply.github.com>
2023-03-31 23:40:06 +0300

    Update Model.java

--- src/main/java/kodlama/io/ecommerce/entities/Model.java
+++ src/main/java/kodlama/io/ecommerce/entities/Model.java
@@ -24,6 +24,6 @@ public class Model {
     @JsonManagedReference
     private Brand brand;
     @OneToMany(mappedBy = "model")
-    @JsonManagedReference
+    @JsonBackReference
     private List<Phone> phones;
 }
